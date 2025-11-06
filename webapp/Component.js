066dc43e235154520acaa08b481a1e7a0355af20
chkvdm <vadikvr6@outlook.com>
2025-11-06 20:32:32 +0400

    refactor: (step-10) add manifest.json descriptor for app configuration

--- webapp/Component.js
+++ webapp/Component.js
@@ -4,17 +4,13 @@ sap.ui.define(
     'sap/ui/model/json/JSONModel',
     'sap/ui/model/resource/ResourceModel',
   ],
-  (UIComponent, JSONModel, ResourceModel) => {
+  (UIComponent, JSONModel) => {
     'use strict';
 
     return UIComponent.extend('ui5.walkthrough.Component', {
       metadata: {
         interfaces: ['sap.ui.core.IAsyncContentCreation'],
-        rootView: {
-          viewName: 'ui5.walkthrough.view.App',
-          type: 'XML',
-          id: 'app',
-        },
+        manifest: 'json',
       },
 
       init() {
@@ -29,12 +25,6 @@ sap.ui.define(
         };
         const oModel = new JSONModel(oData);
         this.setModel(oModel);
-
-        //set i18n model
-        const i18nModel = new ResourceModel({
-          bundleName: 'ui5.walkthrough.i18n.i18n',
-        });
-        this.setModel(i18nModel, 'i18n');
       },
     });
   }
