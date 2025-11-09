c62c69c023ad50ebc8d62a2e92bfec9b98871a49
chkvdm <vadikvr6@outlook.com>
2025-11-09 14:36:23 +0400

    test: (step-27) test(step-27): configure QUnit and add unit tests

--- webapp/test/initMockServer.js
+++ webapp/test/initMockServer.js
@@ -5,8 +5,3 @@ sap.ui.define(['../localService/mockserver'], (mockserver) => {
 
   sap.ui.require(['sap/ui/core/ComponentSupport']);
 });
-
-// import mockserver from '../localService/mockserver.js';
-// import 'sap/ui/core/ComponentSupport';
-
-// mockserver.init();
